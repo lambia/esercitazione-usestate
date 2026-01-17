f3b820e2dba7cb9cef838528e88306d00a504049
Luca Lambia <luca@lambia.it>
2026-01-17 13:09:18 +0100

    milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+import languages from "./data/languages";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [selectedLang, setSelectedLang] = useState(0)
 
-  return (
-    <div>MyApp</div>
-  )
+  function onElementoCliccato(chi) {
+    console.log("cliccato", chi);
+    setSelectedLang(chi);
+  }
+
+  return <div class="mega-wrapper">
+
+    <h1>Linguaggi</h1>
+
+    {languages.map((language, indice) =>
+      <button onClick={() => { onElementoCliccato(indice) }}>{language.title}</button>
+    )}
+
+    <div>
+      <h2>{languages[selectedLang].title}</h2>
+      <p>{languages[selectedLang].description}</p>
+    </div>
+
+  </div>
 }
 
 export default App
